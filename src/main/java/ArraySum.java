2affad0d60bfcd2fb0a8c86d45c313cb0d774a6d
roger1292 <roger1292@gmail.com>
2023-02-24 03:12:28 +0000

    complete

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -6,8 +6,11 @@ public class ArraySum {
      * @param arr an array of ints
      * @return the sum of all values in arr.
      */
+    int sum = 0;
     public int sum(int[] arr){
-
-        return 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
     }
 }
